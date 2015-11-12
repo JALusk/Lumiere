69a417c421d774c4998ae721b85211a60757ce85
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2015-11-12 16:08:40 -0600

    Add derivative of BB flux with temperature
    Added functions which calculate the derivative of the blackbody flux
    with temperature (with units and without units)

--- fit_blackbody.py
+++ fit_blackbody.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.optimize import curve_fit
 from astropy import units as u
-from planck import planck_function
+from planck import planck_function, dplanck_dT
 
 def bb_flux(wavelength, temperature, angular_radius):
     bb_flux = (np.pi) * planck_function(wavelength, temperature) * (angular_radius)**2
@@ -13,6 +13,16 @@ def bb_flux_nounits(wavelength, temperature, angular_radius):
 
     return flux.value
 
+def dBB_dT(wavelength, temperature, angular_radius):
+    dBB_dT = (np.pi) * dplanck_dT(wavelength, temperature) * (angular_radius)**2
+
+    return dBB_dT
+
+def dBB_dT_nounits(wavelength, temperature, angular_radius):
+    dBB_dT_nounits = dBB_dT(wavelength, temperature, angular_radius)
+
+    return dBB_dT_nounits
+
 def calculate_chisq(y_data, y_data_uncertainties, x_data, func, parameters):
     chisq = np.sum(((y_data - func(x_data, *parameters))/y_data_uncertainties)**2)
     return chisq
@@ -23,7 +33,4 @@ def bb_fit_parameters(wavelengths, fluxes, flux_uncertainties):
     angular_radius = popt[1]
     perr = np.sqrt(np.diag(pcov))
     
-
-    chisq = calculate_chisq(fluxes, flux_uncertainties, wavelengths, bb_flux_nounits, popt)
-
     return temperature, angular_radius, perr
