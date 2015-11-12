e171fa80adf28a498b2082535d1c96d1f774dc52
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2015-11-12 00:19:43 -0600

    Add derivative of the planck function
    For calculating the uncertainties in the planck function, I need to have
    a function to calculate the derivative of the planck function with
    temperature.

--- planck.py
+++ planck.py
@@ -24,3 +24,25 @@ def planck_function(wavelength, temperature):
     B_lambda = B_lambda.to(u.erg / (u.s * u.cm**2 * u.AA * u.sr))
 
     return B_lambda
+
+def dplanck_dT(wavelength, temperature):
+    """Partial derivative of the Planck function with Temperature
+
+    Args:
+        wavelength: Wavelength in Angstrom
+        temperature: Temperature in Kelvin
+
+    Returns:
+        dB_lambda_dT: derivative of the specific intensity of the Planck function in erg s^-1 cm^-2 sterad^-1 AA^-1 K^-1
+    """
+    wavelength = u.Quantity(wavelength, unit=u.Angstrom)
+    temperature = u.Quantity(temperature, unit=u.K)
+
+    C1 = 2.0 * const.h.cgs * const.c.cgs**2
+    C2 = const.h.cgs * const.c.cgs / const.k_B.cgs
+
+    exp_term = np.exp(C2 / (wavelength * temperature))
+    dB_lambda_dT = (C1 * C2 * exp_term) / (wavelength**6 * temperature**2 * (exp_term - 1))
+    dB_lambda_dT = dB_lambda_dT.to(u.erg / (u.s * u.cm**2 * u.AA * u.sr * u.K))
+
+    return dB_lambda_dT
