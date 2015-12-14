abedc5fc4ca1816a8f1e902c95a77e6c14de09e0
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2015-12-13 20:58:56 -0600

    Remove old method of handling filter parameters
    The filter parameters used to be held in a yaml file, requiring a
    function to build the flux and wavelength arrays. This is no longer
    needed

--- mag2flux.py
+++ mag2flux.py
@@ -1,36 +1,6 @@
 import numpy as np
 from astropy import units as u
 
-def get_filter_parameters(filter_band):
-    """Fetches effective wavelength and flux zeropoint of a filter.
-
-    Args:
-        filter_band: StringType - Name of the filter band (ex: 'U').
-
-    Returns:
-        A tuple containing two astropy quantities: the effective wavelength
-        in Angstroms, and the flux at zero magnitudes in erg/s/cm^s/A.
-
-        (effective_wl, flux_at_zero_mag).
-
-    Raises:
-        TypeError: The argument given is not a string.
-        KeyError: The argument given is not a valid filter name.
-    """
-    if not isinstance(filter_band, str):
-        raise TypeError('filter_band must be StringType')
-    elif filter_band in filter_data:
-        effective_wl = filter_data[filter_band]['effective_wl']
-        flux_at_zero_mag = filter_data[filter_band]['flux_at_zero_mag']
-    else:
-        raise KeyError('Your specified filter (%s) is not available', filter_band)
-   
-    # Attach CGS units to parameters
-    effective_wl = effective_wl * u.AA
-    flux_at_zero_mag = flux_at_zero_mag * (u.erg / (u.s * u.cm**2 * u.AA)) 
-
-    return effective_wl, flux_at_zero_mag
-
 def mag2flux(magnitude, uncertainty, effective_wl, flux_at_zero_mag):
     """Converts an observed magnitude in a filter band to an average flux.
 
