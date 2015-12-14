fd9d049d1506bf575cb8e025a5610ed4bb391e98
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2015-12-13 19:07:18 -0600

    Change import statement to load planck module

--- test/test_planck.py
+++ test/test_planck.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 from astropy import constants as const
 from astropy import units as u
-from snobol.planck import planck_function
+from planck import planck_function
 
 class TestPlanckFunctionExtrema(unittest.TestCase):
 
