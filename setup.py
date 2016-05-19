2ab83d6d3bdadbffb81ccb6b2a098c14a46cc9a7
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2016-05-19 11:22:21 -0500

    Add setup.py and rename hdf5 directory

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SNoBoL',
+    version='1.0',
+    description='Supernova Bolometric Lightcurves',
+    author='Jeremy A. Lusk',
+    author_email='jeremy.lusk@gmail.com',
+    url='https://github.com/JALusk/SNoBoL',
+    packages=['snobol',],
+    package_data=['data/sn_data.h5']
+    license='MIT License',
+    )   
