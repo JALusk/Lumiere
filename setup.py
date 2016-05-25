33b85b028fae6f0d6a63448fde9727875fe97423
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2016-05-25 14:26:15 -0500

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,14 @@ setup(
     packages=['snobol'],
     package_data={'snobol' : ['data/sn_data.h5']},
     license='MIT License',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        ],
     install_requires=[
         "astropy",
         "scipy>=0.14",
