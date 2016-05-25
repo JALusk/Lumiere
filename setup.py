e96c93e9f0f51ac057ed904e928cf99518dade0d
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2016-05-25 15:24:02 -0500

    Add new package name to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='SNoBoL',
+    name='SuperBoL',
     version='0.2.1',
     description='Supernova Bolometric Lightcurves',
     author='Jeremy A. Lusk',
     author_email='jeremy.lusk@gmail.com',
-    url='https://github.com/JALusk/SNoBoL',
+    url='https://github.com/JALusk/SuperBoL',
     packages=['snobol'],
     package_data={'snobol' : ['data/sn_data.h5']},
     license='MIT License',
