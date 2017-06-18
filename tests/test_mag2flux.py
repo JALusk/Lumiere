7f44e4098afb63572d87f3be4280eccbfacf6136
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2017-06-17 20:34:40 -0500

    Observation converts nonzero B magnitude to flux
    Works equally well for B and U magnitudes. At this point, the feature is
    complete and the Observation object can convert a magnitude to a flux,
    given a flux conversion factor

--- tests/test_mag2flux.py
+++ tests/test_mag2flux.py
@@ -28,3 +28,12 @@ class TestObservation(unittest.TestCase):
         my_obs = mag2flux.Observation(magnitude, flux_conversion_factor)
         flux = my_obs.convert_to_flux()
         self.assertEqual(flux, expected_flux)
+
+    def test_observation_converts_Johnson_B_nonzero_magnitude_to_flux(self):
+        """Observation should convert magnitude of B = 18.793 to flux"""
+        magnitude = 18.793
+        flux_conversion_factor = 632.0E-11
+        expected_flux = flux_conversion_factor * 10**(-0.4 * magnitude)
+        my_obs = mag2flux.Observation(magnitude, flux_conversion_factor)
+        flux = my_obs.convert_to_flux()
+        self.assertEqual(flux, expected_flux)
