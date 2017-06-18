61f035682d3024064d1a300f0523e023d0b6ccc0
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2017-06-17 20:16:28 -0500

    Observation converts U = 0 and B = 0 to flux
    The Observation method now takes the flux conversion factor as a
    parameter, and simply returns it. This won't work for any magnitude
    other than 0, so that will be the next test.

--- superbol/mag2flux.py
+++ superbol/mag2flux.py
@@ -1,7 +1,8 @@
 class Observation(object):
 
-    def __init__(self, magnitude):
+    def __init__(self, magnitude, flux_conversion_factor):
         self.magnitude = magnitude
+        self.flux_conversion_factor = flux_conversion_factor
 
     def convert_to_flux(self):
-        return 417.5E-11
+        return self.flux_conversion_factor
--- tests/test_mag2flux.py
+++ tests/test_mag2flux.py
@@ -8,8 +8,15 @@ class TestObservation(unittest.TestCase):
         """Observation should convert magnitude of U = 0 to the flux conversion factor of the filter"""
         magnitude = 0
         flux_conversion_factor = 417.5E-11
-        my_obs = mag2flux.Observation(magnitude)
+        my_obs = mag2flux.Observation(magnitude, flux_conversion_factor)
         flux = my_obs.convert_to_flux()
         self.assertEqual(flux, flux_conversion_factor)
 
+    def test_observation_converts_Johsnon_B_zero_magnitude_to_flux(self):
+        """Observation should convert magnitude of B = 0 to the flux conversion factor of the filter"""
+        magnitude = 0
+        flux_conversion_factor = 632.0E-11
+        my_obs = mag2flux.Observation(magnitude, flux_conversion_factor)
+        flux = my_obs.convert_to_flux()
+        self.assertEqual(flux, flux_conversion_factor)
         
