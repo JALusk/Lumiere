f9d81e0f0c8423c15526a8c11284d63350817d78
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2017-06-17 20:11:08 -0500

    Refactor: don't need initial test anymore

--- tests/test_mag2flux.py
+++ tests/test_mag2flux.py
@@ -4,9 +4,6 @@ from superbol import mag2flux
 
 class TestObservation(unittest.TestCase):
 
-    def test_observation(self):
-        my_obs = mag2flux.Observation(0)
-
     def test_observation_converts_Johnson_U_zero_magnitude_to_flux(self):
         """Observation should convert magnitude of U = 0 to the flux conversion factor of the filter"""
         magnitude = 0
@@ -14,4 +11,5 @@ class TestObservation(unittest.TestCase):
         my_obs = mag2flux.Observation(magnitude)
         flux = my_obs.convert_to_flux()
         self.assertEqual(flux, flux_conversion_factor)
+
         
