188ec56bfdde8c09dec173db502d928c17c1be59
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2017-06-27 21:36:04 -0500

    Implement trapezoidal integration and uncertainty
    The trapezoidal integration of a list of fluxes is up and running. I
    chose to create a class to handle the integration, that way I can change
    the implementation of the integral calculation (for example, to do the
    spline fit that Taddia et. al. do) later down the line.

--- superbol/lqbol.py
+++ superbol/lqbol.py
@@ -1,4 +1,55 @@
-class SpectralEnergyDistribution(object):
+import math
+import numpy as np
 
-    def sort_by_wavelength(self, fluxes):
-        return sorted(fluxes, key=lambda flux: flux.wavelength)
+class InsufficientFluxes(Exception):
+    pass
+
+class QuasiBolometricFlux(object):
+    def __init__(self, value, uncertainty):
+        self.value = value
+        self.uncertainty = uncertainty
+
+class TrapezoidalIntegralCalculator(object):
+    """Integrate between fluxes using the trapezoidal method"""
+    def _get_flux_list(self, fluxes):
+        """Return a list of flux values"""
+        return [f.flux for f in fluxes]
+
+    def _get_wavelength_list(self, fluxes):
+        """Return a list of flux wavelengths"""
+        return [f.wavelength for f in fluxes]
+
+    def calculate(self, fluxes):
+        """Calculate the integral using numpy.trapz"""
+        flux_list = self._get_flux_list(fluxes)
+        wavelength_list = self._get_wavelength_list(fluxes)
+        return np.trapz(flux_list, wavelength_list)
+
+def get_quasi_bolometric_flux(integral_calculator, 
+                              uncertainty_calculator, 
+                              fluxes):
+    if len(fluxes) < 2:
+        raise InsufficientFluxes(
+            "Cannot calculate quasi-bolometric flux with fewer " +
+            "than two fluxes, {0} received".format(len(fluxes)))
+
+    return QuasiBolometricFlux(
+            value = integral_calculator.calculate(fluxes),
+            uncertainty = uncertainty_calculator(fluxes))
+
+def uncertainty_calculator_trapezoidal(fluxes):
+    """Calculate uncertainty in trapezoidal integral of fluxes"""
+    radicand = 0
+
+    for i, flux in enumerate(fluxes):
+        if i == 0:
+            radicand += (0.5 * (fluxes[i+1].wavelength - flux.wavelength) 
+                         * flux.flux_uncertainty)**2
+        elif i == len(fluxes) - 1:
+            radicand += (0.5 * (flux.wavelength - fluxes[i-1].wavelength)
+                         * flux.flux_uncertainty)**2
+        else:
+            radicand += (0.5 * (fluxes[i+1].wavelength - fluxes[i-1].wavelength)
+                         * flux.flux_uncertainty)**2
+
+    return math.sqrt(radicand)
--- tests/test_lqbol.py
+++ tests/test_lqbol.py
@@ -1,28 +1,147 @@
 import unittest
 import math
+
+from unittest.mock import Mock
+
 from .context import superbol
 from superbol import mag2flux
 from superbol import lqbol
 
-class TestSpectralEnergyDistribution(unittest.TestCase):
-
+class TestGetQuasiBolometricFlux(unittest.TestCase):
+    """When obtaining the trapezoidal integration result"""
     def setUp(self):
-        self.sed = lqbol.SpectralEnergyDistribution()
+        self.integral_calculator = Mock()
+        self.uncertainty_calculator = Mock()
 
-    def test_sort_dummy_flux_objects_by_wavelength(self):
-        flux2 = mag2flux.MonochromaticFlux(300, 10, 2)
-        flux1 = mag2flux.MonochromaticFlux(200, 30, 1)
-        flux3 = mag2flux.MonochromaticFlux(100, 20, 3)
-        fluxes = [flux2, flux1, flux3]
-        expected = [flux1, flux2, flux3]
-        result = self.sed.sort_by_wavelength(fluxes)
-        self.assertEqual(expected, result)
+    def test_no_fluxes(self):
+        """it raises an exception with an empty flux set"""
+        with self.assertRaises(lqbol.InsufficientFluxes):
+            lqbol.get_quasi_bolometric_flux(
+                integral_calculator = self.integral_calculator,
+                uncertainty_calculator = self.uncertainty_calculator,
+                fluxes=[])
 
-    def test_sort_real_flux_objects_by_wavelength(self):
-        flux1 = mag2flux.MonochromaticFlux(1.921E-16, 2.654E-18, 4380.0)
-        flux2 = mag2flux.MonochromaticFlux(7.555E-17, 2.505E-18, 3660.0)
-        flux3 = mag2flux.MonochromaticFlux(2.132E-16, 2.946E-18, 5450.0)
-        fluxes = [flux1, flux2, flux3]
-        expected = [flux2, flux1, flux3]
-        result = self.sed.sort_by_wavelength(fluxes)
-        self.assertEqual(expected, result)
+    def test_one_flux(self):
+        """it raises an exception with a single flux"""
+        flux = mag2flux.MonochromaticFlux(flux = 200,
+                                          flux_uncertainty = 30,
+                                          wavelength = 1)
+
+        with self.assertRaises(lqbol.InsufficientFluxes):
+            lqbol.get_quasi_bolometric_flux(
+                integral_calculator = self.integral_calculator,
+                uncertainty_calculator = self.uncertainty_calculator,
+                fluxes=[flux])
+
+    def test_two_fluxes(self):
+        """it produces quasi-bolometric flux with more than one flux"""
+        flux1 = mag2flux.MonochromaticFlux(flux = 100,
+                                           flux_uncertainty = 0,
+                                           wavelength = 0)
+
+        flux2 = mag2flux.MonochromaticFlux(flux = 100,
+                                           flux_uncertainty = 0,
+                                           wavelength= 1 )
+
+        two_fluxes = [flux1, flux2]
+
+        self.integral_calculator.calculate = Mock(return_value = 100)
+        self.uncertainty_calculator = Mock(return_value = 10)
+        
+        expected_value = 100
+        expected_uncertainty = 10
+
+        result = lqbol.get_quasi_bolometric_flux(
+            integral_calculator = self.integral_calculator,
+            uncertainty_calculator = self.uncertainty_calculator,
+            fluxes = two_fluxes)
+
+        self.uncertainty_calculator.assert_called_once_with(two_fluxes)
+        self.integral_calculator.calculate.assert_called_once_with(two_fluxes)
+        self.assertEqual(expected_value, result.value)
+        self.assertEqual(expected_uncertainty, result.uncertainty)
+
+class TestUncertaintyCalculatorTrapezoidal(unittest.TestCase):
+
+    def test_equal_flux_zero_uncertainty(self):
+        flux1 = mag2flux.MonochromaticFlux(flux = 100,
+                                           flux_uncertainty = 0,
+                                           wavelength = 0)
+
+        flux2 = mag2flux.MonochromaticFlux(flux = 100,
+                                           flux_uncertainty = 0,
+                                           wavelength= 1)
+
+        expected_uncertainty = 0
+        result = lqbol.uncertainty_calculator_trapezoidal(fluxes=[flux1, flux2])
+        self.assertEqual(expected_uncertainty, result)
+
+    def test_equal_flux_equal_uncertainty(self):
+        flux1 = mag2flux.MonochromaticFlux(flux = 100,
+                                           flux_uncertainty = 10,
+                                           wavelength = 0)
+
+        flux2 = mag2flux.MonochromaticFlux(flux = 100,
+                                           flux_uncertainty = 10,
+                                           wavelength= 1)
+
+        expected_uncertainty = math.sqrt(50)
+        result = lqbol.uncertainty_calculator_trapezoidal(fluxes=[flux1,flux2])
+        self.assertEqual(expected_uncertainty, result)
+
+    def test_unequal_flux_unequal_uncertainty(self):
+        flux1 = mag2flux.MonochromaticFlux(flux = 100,
+                                           flux_uncertainty = 10,
+                                           wavelength = 0)
+
+        flux2 = mag2flux.MonochromaticFlux(flux = 200,
+                                           flux_uncertainty = 20,
+                                           wavelength= 1)
+
+        expected_uncertainty = math.sqrt(125)
+        result = lqbol.uncertainty_calculator_trapezoidal(fluxes=[flux1,flux2])
+        self.assertEqual(expected_uncertainty, result)
+
+    def test_three_fluxes(self):
+        flux1 = mag2flux.MonochromaticFlux(flux = 100,
+                                           flux_uncertainty = 10,
+                                           wavelength = 0)
+
+        flux2 = mag2flux.MonochromaticFlux(flux = 200,
+                                           flux_uncertainty = 20,
+                                           wavelength= 1)
+
+        flux3 = mag2flux.MonochromaticFlux(flux = 150,
+                                           flux_uncertainty = 8,
+                                           wavelength= 2)
+
+        expected_uncertainty = math.sqrt(441)
+        result = lqbol.uncertainty_calculator_trapezoidal(fluxes=[flux1,flux2,flux3])
+        self.assertEqual(expected_uncertainty, result)
+
+class TestTrapezoidalIntegralCalculator(unittest.TestCase):
+    
+    def setUp(self):
+        self.integral_calculator = lqbol.TrapezoidalIntegralCalculator()
+        self.flux1 = mag2flux.MonochromaticFlux(flux = 100,
+                                                flux_uncertainty = 10,
+                                                wavelength = 0)
+        self.flux2 = mag2flux.MonochromaticFlux(flux = 200,
+                                                flux_uncertainty = 20,
+                                                wavelength= 1)
+        self.flux3 = mag2flux.MonochromaticFlux(flux = 150,
+                                                flux_uncertainty = 8,
+                                                wavelength= 2)
+        self.fluxes = [self.flux1, self.flux2, self.flux3]
+    
+    def test_flux_list(self):
+        flux_list = self.integral_calculator._get_flux_list(self.fluxes)
+        self.assertEqual([100, 200, 150], flux_list)
+
+    def test_wavelength_list(self):
+        wl_list = self.integral_calculator._get_wavelength_list(self.fluxes)
+        self.assertEqual([0, 1, 2], wl_list)
+
+    def test_trapezoidal_integral(self):
+        integral = self.integral_calculator.calculate(self.fluxes)
+        self.assertEqual(325, integral)
