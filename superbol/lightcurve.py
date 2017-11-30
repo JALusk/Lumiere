94854a9e2be9d21210e5d37951ad12b3c5917f1d
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2017-11-30 14:00:56 -0600

    Move flux grouping code to SED module

--- superbol/lightcurve.py
+++ superbol/lightcurve.py
@@ -1,19 +1,9 @@
 import math
 
-from itertools import groupby
-from superbol import lqbol
-
-
-def group_fluxes(fluxes, keyfunc=None):
-    """Group fluxes by applying keyfunc() to each flux.time in the fluxes.
-
-    Fluxes with the same return value for keyfunc(flux.time) will be 
-    part of the same group."""
-    grouped_fluxes = [list(it) for k, it in groupby(fluxes, lambda x: keyfunc(x.time))]
-    return grouped_fluxes
+from superbol import sed
 
 def calculate_lightcurve(fluxes, distance, luminosity_calculator):
-    grouped_fluxes = group_fluxes(fluxes, math.floor)
+    grouped_fluxes = sed.group_fluxes(fluxes, math.floor)
     lightcurve = []
     for flux_group in grouped_fluxes:
         if len(flux_group) > 2:
