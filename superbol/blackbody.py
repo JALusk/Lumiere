5c0faf439e0236b3c0a305afcdacb7a56d75b22b
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2018-04-23 15:00:37 -0500

    Add BlackbodyFit object
    The BlackbodyFit object will be the basis of blackbody fitting, with
    external functions handling the IR and UV corrections

--- superbol/blackbody.py
+++ superbol/blackbody.py
@@ -28,6 +28,28 @@ def bb_flux_nounits(wavelength, temperature, angular_radius):
 
     return flux.value
 
+def bb_total_flux(self):
+    """Integrate the planck function from :math:`\\lambda = 0` to
+       :math:`\\lambda = \\infty`, then convert result to observed flux.
+
+    The calculation is done using the Stefan-Boltxmann law, rather than 
+    performing an actual integral using numerical integration. The 
+    `angular_radius` is included to convert the result to an observed flux.
+
+    Args:
+        temperature (float): Temperature in Kelvin
+        angular_radius (float): Angular radius :math:`(\\theta = \\frac{R}{D})`
+
+    Returns:
+        float: The value of :math:`\\sigma \\frac{R^2}{D^2} T^4`
+    """
+    temperature = u.Quantity(self.temperature, u.K)
+    bb_total_flux = (const.sigma_sb * self.angular_radius**2
+                     * self.temperature**4)
+    bb_total_flux = bb_total_flux.to(u.erg / (u.s * u.cm**2))
+    return bb_total_flux.value 
+
+
 def bb_flux_integrated(wavelength, temperature, angular_radius):
     """Integrate the planck function from :math:`\\lambda = 0` to :math:`\\lambda =` `wavelength`, then convert result to observed flux
     
@@ -58,23 +80,6 @@ def dbb_flux_integrated_dT(wavelength, temperature, angular_radius):
 
     return dbb_flux_integrated_dT.value
 
-def bb_total_flux(temperature, angular_radius):
-    """Integrate the planck function from :math:`\\lambda = 0` to :math:`\\lambda = \\infty`, then convert result to observed flux.
-
-    The calculation is done using the Stefan-Boltxmann law, rather than performing an actual integral using numerical integration. The `angular_radius` is included to convert the result to an observed flux.
-
-    Args:
-        temperature (float): Temperature in Kelvin
-        angular_radius (float): Angular radius :math:`(\\theta = \\frac{R}{D})`
-
-    Returns:
-        float: The value of :math:`\\sigma \\frac{R^2}{D^2} T^4`
-    """
-    temperature = u.Quantity(temperature, u.K)
-    bb_total_flux = const.sigma_sb * (angular_radius**2) * temperature**4
-    bb_total_flux = bb_total_flux.to(u.erg / (u.s * u.cm**2))
-    return bb_total_flux.value
-
 def dbb_total_flux_dT(temperature, angular_radius):
     """Take the derivative of the total blackbody flux with respect to temperature
 
@@ -92,25 +97,54 @@ def dbb_total_flux_dT(temperature, angular_radius):
     bb_total_flux = bb_total_flux.to(u.erg / (u.s * u.cm**2 * u.K))
     return bb_total_flux.value
 
-def bb_fit_parameters(SED):
-    """Fit a blackbody to the observed `SED`.
+class BlackbodyFit(object):
+
+    def __init__(self):
+        self.temperature = None
+        self.angular_radius = None
+        self.temperature_err = None
+        self.angular_radius_err = None
+        self.SED = None
+
+    def fit_to_SED(self, SED):
+        """Fits the temperature and angular_radius of a blackbody to the SED
+        
+        The initial guesses for the `temperature` and `angular_radius` are
+        :math:`T = 5000` K and :math:`\\theta = 1.0 \\times 10^{-10}`.
+        These are typical for an extragalactic supernovae, but should be used
+        with caution for any other objects.
     
-    The initial guesses for the `temperature` and `angular_radius` are :math:`T = 5000` K and :math:`\\theta = 1.0 \\times 10^{-10}`. These are typical for an extragalactic supernovae, but should be used with caution for any other objects.
+        Args:
+            SED (list): List of observed MonochromaticFlux objects.
+        """
+        wavelengths = [f.wavelength for f in SED]
+        fluxes = [f.flux for f in SED]
+        flux_uncertainties = [f.flux_uncertainty for f in SED]
 
-    Args:
-        SED (list): List of observed MonochromaticFlux objects.
+        popt, pcov = curve_fit(bb_flux_nounits, wavelengths, fluxes,
+                               p0=[5000, 1.0e-10], sigma=flux_uncertainties,
+                               absolute_sigma=True)
+       
+        self.SED = SED
+        self.temperature = popt[0]
+        self.angular_radius = popt[1]
 
-    Returns:
-        tuple: Tuple containing the best fit `temperature`, `angular_radius`, as well as perr, a 2-tuple containing the `temperature` error and the `angular_radius` error.
+        perr = np.sqrt(np.diag(pcov))
+        self.temperature_err = perr[0]
+        self.angular_radius_err = perr[1]
 
-        (temperature, angular_radius, perr)
-    """
-    wavelengths = [f.wavelength for f in SED]
-    fluxes = [f.flux for f in SED]
-    flux_uncertainties = [f.flux_uncertainty for f in SED]
-    popt, pcov = curve_fit(bb_flux_nounits, wavelengths, fluxes, p0=[5000, 1.0e-10], sigma=flux_uncertainties, absolute_sigma=True)
-    temperature = popt[0]
-    angular_radius = popt[1]
-    perr = np.sqrt(np.diag(pcov))
-    
-    return temperature, angular_radius, perr
+        
+    def __call__(self, wavelength):
+        """Flux of the blackbody at a given wavelength in angstroms
+        
+        Args:
+            wavelength (float): Wavelength in Angstroms
+            
+        Returns:
+            bb_flux (float): blackbody flux at given wavelength in cgs units"""
+        if self.temperature == None:
+            return None
+        else:
+            bb_flux = (np.pi * u.sr * planck_function(wavelength, self.temperature)
+                       * self.angular_radius**2)
+            return bb_flux.value
