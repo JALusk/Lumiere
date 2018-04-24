172b271aee591edf4c0b1ee5ef7bd65e3f4f6144
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2018-04-24 09:06:56 -0500

    Re-arrange code in SED module for clarity
    Trying to put the highest-level code closer to the top, with the details
    following.

--- superbol/sed.py
+++ superbol/sed.py
@@ -9,40 +9,20 @@ from scipy.interpolate import interp1d
 class MissingFluxOutOfBounds(Exception):
     pass
 
-def group_fluxes(fluxes, keyfunc=math.floor):
-    """Group fluxes by applying keyfunc() to each flux.time in the fluxes.
+def get_SED(fluxes):
+    """Return a list of MonochromaticFluxes with duplicates averaged"""
+    SED = []
+    for f in yield_fluxes_at_each_observed_wavelength(fluxes):
+        if len(f) == 1:
+            SED.append(f[0])
+        else:
+            SED.append(combine_fluxes(f))
+    return SED
 
-    Fluxes with the same return value for keyfunc(flux.time) will be 
-    part of the same group."""
-    grouped_fluxes = [list(it) for k, it in groupby(fluxes, lambda x: keyfunc(x.time))]
-    return grouped_fluxes
-
-def get_weights(uncertainties):
-    """Calculate weights from uncertainties"""
-    weights = [1/s**2 for s in uncertainties]
-    return weights
-
-def weighted_average(values, uncertainties):
-    """Calculate the weighed average of values with uncertainties"""
-    weights = get_weights(uncertainties)
-    denominator = sum(weights)
-    numerator = sum([weights[i] * values[i] for i in range(len(values))])
-    return numerator/denominator
-
-def weighted_average_uncertainty(uncertainties):
-    """Calculate the uncertainty in a weighted average"""
-    weights = get_weights(uncertainties)
-    uncertainty = 1.0/math.sqrt(sum(weights))
-    return uncertainty
-
-def get_flux_values(fluxes):
-    return [f.flux for f in fluxes]
-
-def get_flux_uncertainties(fluxes):
-    return [f.flux_uncertainty for f in fluxes]
-
-def get_times(fluxes):
-    return [f.time for f in fluxes]
+def yield_fluxes_at_each_observed_wavelength(fluxes):
+    """Yield lists of MonochromaticFluxes with the same wavelength"""
+    for wavelength in set(f.wavelength for f in fluxes):
+        yield [f for f in fluxes if f.wavelength == wavelength]
 
 def combine_fluxes(fluxes):
     """Combine a list of MonochromaticFluxes using a weighted average"""
@@ -57,20 +37,41 @@ def combine_fluxes(fluxes):
                                       wavelength,
                                       time)
 
-def yield_fluxes_at_each_observed_wavelength(fluxes):
-    """Yield lists of MonochromaticFluxes with the same wavelength"""
-    for wavelength in set(f.wavelength for f in fluxes):
-        yield [f for f in fluxes if f.wavelength == wavelength]
+def get_flux_values(fluxes):
+    return [f.flux for f in fluxes]
 
-def get_SED(fluxes):
-    """Return a list of MonochromaticFluxes with duplicates averaged"""
-    SED = []
-    for f in yield_fluxes_at_each_observed_wavelength(fluxes):
-        if len(f) == 1:
-            SED.append(f[0])
-        else:
-            SED.append(combine_fluxes(f))
-    return SED
+def get_flux_uncertainties(fluxes):
+    return [f.flux_uncertainty for f in fluxes]
+
+def get_times(fluxes):
+    return [f.time for f in fluxes]
+
+def weighted_average(values, uncertainties):
+    """Calculate the weighed average of values with uncertainties"""
+    weights = get_weights(uncertainties)
+    denominator = sum(weights)
+    numerator = sum([weights[i] * values[i] for i in range(len(values))])
+    return numerator/denominator
+
+def weighted_average_uncertainty(uncertainties):
+    """Calculate the uncertainty in a weighted average"""
+    weights = get_weights(uncertainties)
+    uncertainty = 1.0/math.sqrt(sum(weights))
+    return uncertainty
+
+def get_weights(uncertainties):
+    """Calculate weights from uncertainties"""
+    weights = [1/s**2 for s in uncertainties]
+    return weights
+
+
+def group_fluxes(fluxes, keyfunc=math.floor):
+    """Group fluxes by applying keyfunc() to each flux.time in the fluxes.
+
+    Fluxes with the same return value for keyfunc(flux.time) will be 
+    part of the same group."""
+    grouped_fluxes = [list(it) for k, it in groupby(fluxes, lambda x: keyfunc(x.time))]
+    return grouped_fluxes
 
 def interpolate_missing_fluxes(SEDs):
     observed_wavelengths = get_observed_wavelengths(SEDs)
