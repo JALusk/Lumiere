5274a016a7ee0bb912efbfe966ea2dea647f4f3e
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2018-04-24 15:38:18 -0500

    Update names and docstring in the photometry module
    I didn't have the names switched over from when I copied this from the
    SED module

--- superbol/photometry.py
+++ superbol/photometry.py
@@ -2,10 +2,10 @@ import math
 
 from itertools import groupby
 
-def group_magnitudes(fluxes, keyfunc=math.floor):
-    """Group fluxes by applying keyfunc() to each flux.time in the fluxes.
+def group_magnitudes(magnitudes, keyfunc=math.floor):
+    """Group magnitudes by applying keyfunc() to each magnitude.time.
 
-    Fluxes with the same return value for keyfunc(flux.time) will be 
+    Magnitudes with the same return value for keyfunc(magnitude.time) will be 
     part of the same group."""
-    grouped_fluxes = [list(it) for k, it in groupby(fluxes, lambda x: keyfunc(x.time))]
-    return grouped_fluxes
+    grouped_magnitudes = [list(it) for k, it in groupby(magnitudes, lambda x: keyfunc(x.time))]
+    return grouped_magnitudes
