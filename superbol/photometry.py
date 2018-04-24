b91478871ac24c3090580d8a21f79e7819d171e4
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2018-04-24 15:59:43 -0500

    Add test to combine magnitudes
    This necessitated copying and changing code from the SED module into the
    photometry module. It also necessitated changing the ObservedMagnitude
    object to implement __eq__():

--- superbol/photometry.py
+++ superbol/photometry.py
@@ -1,6 +1,8 @@
 import math
+import numpy as np
 
 from itertools import groupby
+from superbol import mag2flux
 
 def group_magnitudes(magnitudes, keyfunc=math.floor):
     """Group magnitudes by applying keyfunc() to each magnitude.time.
@@ -9,3 +11,41 @@ def group_magnitudes(magnitudes, keyfunc=math.floor):
     part of the same group."""
     grouped_magnitudes = [list(it) for k, it in groupby(magnitudes, lambda x: keyfunc(x.time))]
     return grouped_magnitudes
+
+def combine_magnitudes(observed_magnitudes):
+    """Combine a list of ObservedMagnitudes using a weighted average"""
+    values = get_observed_magnitude_values(observed_magnitudes)
+    uncertainties = get_observed_magnitude_uncertainties(observed_magnitudes)
+    combined_magnitude = weighted_average(values, uncertainties)
+    combined_uncertainty = weighted_average_uncertainty(uncertainties)
+    band = observed_magnitudes[0].band
+    time = observed_magnitudes[0].time
+    return mag2flux.ObservedMagnitude(combined_magnitude,
+                                      combined_uncertainty,
+                                      band,
+                                      time)
+
+def get_observed_magnitude_values(observed_magnitudes):
+    return [obs.magnitude for obs in observed_magnitudes]
+
+def get_observed_magnitude_uncertainties(observed_magnitudes):
+    return [obs.uncertainty for obs in observed_magnitudes]
+
+def weighted_average(values, uncertainties):
+    """Calculate the weighed average of values with uncertainties"""
+    weights = get_weights(uncertainties)
+    denominator = sum(weights)
+    numerator = sum([weights[i] * values[i] for i in range(len(values))])
+    return numerator/denominator
+
+def weighted_average_uncertainty(uncertainties):
+    """Calculate the uncertainty in a weighted average"""
+    weights = get_weights(uncertainties)
+    uncertainty = 1.0/math.sqrt(sum(weights))
+    return uncertainty
+
+def get_weights(uncertainties):
+    """Calculate weights from uncertainties"""
+    weights = [1/s**2 for s in uncertainties]
+    return weights
+
--- tests/unit/test_photometry.py
+++ tests/unit/test_photometry.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 
 from superbol import mag2flux
 from superbol import photometry
@@ -45,3 +46,37 @@ class TestGroupObservedMagnitudes(unittest.TestCase):
         result = photometry.group_magnitudes(magnitudes)
         self.assertEqual(expected, result)
 
+class TestCombineMagnitudes(unittest.TestCase):
+
+    def setUp(self):
+        self.mag1 = mag2flux.ObservedMagnitude(magnitude = 100,
+                                               uncertainty = 10,
+                                               band = 'U',
+                                               time = 0)
+        self.mag2 = mag2flux.ObservedMagnitude(magnitude = 200,
+                                               uncertainty = 10,
+                                               band = 'U',
+                                               time = 0)
+        self.mag3 = mag2flux.ObservedMagnitude(magnitude = 150,
+                                               uncertainty = 8,
+                                               band = 'B',
+                                               time = 0)
+        self.mag4 = mag2flux.ObservedMagnitude(magnitude = 50,
+                                               uncertainty = 8,
+                                               band = 'V',
+                                               time = 0)
+        self.mag5 = mag2flux.ObservedMagnitude(magnitude = 60,
+                                               uncertainty = 8,
+                                               band = 'V',
+                                               time = 0)
+        self.magnitudes = [self.mag1, self.mag2, self.mag3, self.mag4, self.mag5]
+        self.repeated_magnitudes1 = [self.mag1, self.mag2]
+        self.repeated_magnitudes3 = [self.mag4, self.mag5]
+
+    def test_combine_magnitudes_equal_uncertainties(self):
+        result = photometry.combine_magnitudes(self.repeated_magnitudes1)
+        expected = mag2flux.ObservedMagnitude(magnitude = 150,
+                                              uncertainty = np.sqrt(200)/2.,
+                                              band = 'U',
+                                              time = 0)
+        self.assertEqual(expected, result)
