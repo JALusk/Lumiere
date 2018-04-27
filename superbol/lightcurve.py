2f941710f1efbbbc134262d84e3df9b20b165b86
Jeremy A. Lusk <jeremy.lusk@gmail.com>
2018-04-27 13:44:03 -0500

    Add function to calculate BC luminosity
    This is the main interface for calculating a BC luminosity.
    
    In the future, I should get rid of some of this duplication between the
    calculate bc luminosity code and the other luminosity calculation. This
    would probably involve generalizing the methods that are currently
    implemented in the sed and photometry modules into methods that worked
    on 'observations' be they fluxes or magnitudes.

--- superbol/lightcurve.py
+++ superbol/lightcurve.py
@@ -1,6 +1,7 @@
 import math
 
 from superbol import sed
+from superbol import photometry
 
 def calculate_lightcurve(fluxes, distance, luminosity_calculator):
     grouped_fluxes = sed.group_fluxes(fluxes, math.floor)
@@ -14,5 +15,20 @@ def calculate_lightcurve(fluxes, distance, luminosity_calculator):
 
     for SED in SEDs:
         luminosity = luminosity_calculator(SED, distance)
+        lightcurve.append(luminosity)
+    return lightcurve
+
+def calculate_bc_lightcurve(magnitudes, distance, luminosity_calculator):
+    grouped_magnitudes = photometry.group_magnitudes(magnitudes, math.floor)
+    lightcurve = []
+    multi_band_photometry_set = []
+    for magnitude_group in grouped_magnitudes:
+        if len(magnitude_group) > 2:
+            multi_band_photometry_set.append(photometry.get_multi_band_photometry(magnitude_group))
+
+    photometry.interpolate_missing_magnitudes(multi_band_photometry_set)
+
+    for multi_band_photometry in multi_band_photometry_set:
+        luminosity = luminosity_calculator(multi_band_photometry, distance)
         lightcurve.append(luminosity)
     return lightcurve
