db99aeee5a56e90ae946d81f0b9ba7bd3879ee7e
Zachary Humphrey <70534409+zacharyjhumphrey@users.noreply.github.com>
2021-10-26 14:13:23 -0500

    Adding napolean to Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,19 @@ release = '0.3.4'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
 ]
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_use_rtype = True
+
+# Todo settings
+todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
