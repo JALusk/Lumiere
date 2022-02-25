c0f17ca8dcc852fd738071b47bd088bc11c609a5
Zachary Humphrey <70534409+zacharyjhumphrey@users.noreply.github.com>
2022-02-25 16:00:12 -0600

    refactoring space.sne tests

--- superbol/read_sne.py
+++ superbol/read_sne.py
@@ -4,7 +4,7 @@ import requests
 from superbol import lightcurve, lqbol, lum, read_osc
 
 def get_supernova_lum_dist(name):
-    return requests.get(f'https://api.astrocats.space/{name}/lumdist').json()
+    return requests.get(f'https://api.astrocats.space/{name}/lumdist').json()[name]["lumdist"]
 
 def get_supernova_photometry(name):
     return requests.get(f'https://api.astrocats.space/{name}/photometry/').json()[name]["photometry"]
