e5cc4ef447e840f78ec9ff812e79222f5f954882
Hypatia Meraviglia <hbarry@cub.uca.edu>
2022-06-06 23:38:42 -0500

    Plotted linear fit to post-plateau tail of 2018hna lightcurve

--- plot_2018hna_lightcurve.py
+++ plot_2018hna_lightcurve.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 #Get data from file
 with open('luminosity_2018hna.txt', 'r') as f:
@@ -16,8 +17,19 @@ print("Luminosity: ", luminosity)
 log_luminosity = [math.log10(x) for x in luminosity]
 print("Log of luminosity: ", log_luminosity)
 
+#Linear fit to post-plateau decline
+decline_luminosity = log_luminosity[35:]
+decline_time = time[35:]
+fit = np.polyfit(decline_time, decline_luminosity, 1)
+line = np.poly1d(fit)
+print("Line: ", line)
+print("Decline time: ", decline_time)
+print("Fit: ", fit)
+print("Decline luminosity: ", decline_luminosity)
+
 #Plot lightcurve
 plt.scatter(time, log_luminosity)
+plt.plot(decline_time, line(decline_time))
 plt.title("Time since explosion (days) vs. log base 10 of luminosity")
 plt.xlabel('Time since explosion (days)')
 plt.ylabel('Log(luminosity)')
