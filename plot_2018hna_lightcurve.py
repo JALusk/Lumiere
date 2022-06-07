32b2e87b23922be31ec0e9d3787996158f70a257
Hypatia Meraviglia <hbarry@cub.uca.edu>
2022-06-06 23:40:39 -0500

    Cleaned up print statements in plotting script

--- plot_2018hna_lightcurve.py
+++ plot_2018hna_lightcurve.py
@@ -11,21 +11,16 @@ with open('luminosity_2018hna.txt', 'r') as f:
         time.append(float(i.split(',')[1]))
         luminosity.append(float(i.split(',')[3]))
     f.close()
-print("Time: ", time)
-print("Luminosity: ", luminosity)
+
 #Find log of the luminosity
 log_luminosity = [math.log10(x) for x in luminosity]
-print("Log of luminosity: ", log_luminosity)
 
 #Linear fit to post-plateau decline
 decline_luminosity = log_luminosity[35:]
 decline_time = time[35:]
 fit = np.polyfit(decline_time, decline_luminosity, 1)
 line = np.poly1d(fit)
-print("Line: ", line)
-print("Decline time: ", decline_time)
-print("Fit: ", fit)
-print("Decline luminosity: ", decline_luminosity)
+print("Linear fit to post-plateau tail: ", line)
 
 #Plot lightcurve
 plt.scatter(time, log_luminosity)
