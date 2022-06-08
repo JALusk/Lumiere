5d3f717c6b4167838cc863049baea70b27f8be0f
Hypatia Meraviglia <hbarry@cub.uca.edu>
2022-06-07 22:32:58 -0500

    Preparing to reverse engineer fit

--- plot_2018hna_lightcurve.py
+++ plot_2018hna_lightcurve.py
@@ -15,17 +15,25 @@ with open('luminosity_2018hna.txt', 'r') as f:
 #Find log of the luminosity
 log_luminosity = [math.log10(x) for x in luminosity]
 
+#Change days to seconds
+time_s = [x*86400 for x in time]
+
 #Linear fit to post-plateau decline
-decline_luminosity = log_luminosity[35:]
-decline_time = time[35:]
+decline_luminosity = luminosity[35:]
+decline_time = time_s[35:]
 fit = np.polyfit(decline_time, decline_luminosity, 1)
 line = np.poly1d(fit)
 print("Linear fit to post-plateau tail: ", line)
 
+#Calculate nickel mass
+g1 = 1.32*10**-6
+g2 = 1.02*10**-7
+s = ((3.90*10**10)*(e**(g2*t)) + (6.78*10**9)(e**(-g2*t) - e**(g1*t)))
+
 #Plot lightcurve
-plt.scatter(time, log_luminosity)
+plt.scatter(time_s, log_luminosity)
 plt.plot(decline_time, line(decline_time))
-plt.title("Time since explosion (days) vs. log base 10 of luminosity")
-plt.xlabel('Time since explosion (days)')
+plt.title("Time since explosion (seconds) vs. log base 10 of luminosity")
+plt.xlabel('Time since explosion (seconds)')
 plt.ylabel('Log(luminosity)')
 plt.savefig('../Desktop/2018hna_lightcurve.png')
