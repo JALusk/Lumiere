7e39041f688519290945a73f8fe191a6f597ceae
Hypatia Meraviglia <hbarry@cub.uca.edu>
2022-07-05 10:17:02 -0500

    Debugging 2018hna calculations in sed.py and test_qbol_lightcurve.py

--- superbol/lqbol.py
+++ superbol/lqbol.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import scipy
 
 from superbol import mag2flux
 from superbol.lum import BolometricFlux
@@ -53,6 +54,31 @@ class TrapezoidalIntegralCalculator(object):
         """Return a list of flux wavelengths"""
         return [f.wavelength for f in fluxes]
 
+class SplineIntegralCalculator(object):
+    """Integrate between fluxes using a cubic spline"""
+    def calculate(self, fluxes):
+        """Calculate the integral using numpy.trapz"""
+        self._sort_fluxes_by_wavelength(fluxes)
+        flux_list = self._get_flux_list(fluxes)
+        wavelength_list = self._get_wavelength_list(fluxes)
+        print("Wavelength list: ", wavelength_list)
+        print("Flux list: ", flux_list)
+        spline = scipy.interpolate.CubicSpline(wavelength_list, flux_list)
+        return spline.integrate(wavelength_list[0], wavelength_list[-1])
+
+    def _sort_fluxes_by_wavelength(self, fluxes):
+        """Sort the fluxes in-place by wavelength"""
+        fluxes.sort(key = lambda x: x.wavelength)
+
+    def _get_flux_list(self, fluxes):
+        """Return a list of flux values"""
+        return [f.flux for f in fluxes]
+
+    def _get_wavelength_list(self, fluxes):
+        """Return a list of flux wavelengths"""
+        return [f.wavelength for f in fluxes]
+
+
 def uncertainty_calculator_trapezoidal(fluxes):
     """Calculate uncertainty in trapezoidal integral of fluxes"""
     radicand = 0
@@ -70,8 +96,26 @@ def uncertainty_calculator_trapezoidal(fluxes):
 
     return math.sqrt(radicand)
 
+
+def uncertainty_calculator_spline(fluxes):
+    """Calculate uncertainty in spline integral of fluxes"""
+    radicand = 0
+
+    for i, flux in enumerate(fluxes):
+        if i == 0:
+            radicand += (0.5 * (fluxes[i+1].wavelength - flux.wavelength) 
+                         * flux.flux_uncertainty)**2
+        elif i == len(fluxes) - 1:
+            radicand += (0.5 * (flux.wavelength - fluxes[i-1].wavelength)
+                         * flux.flux_uncertainty)**2
+        else:
+            radicand += (0.5 * (fluxes[i+1].wavelength - fluxes[i-1].wavelength)
+                         * flux.flux_uncertainty)**2
+
+    return math.sqrt(radicand)
+
 def calculate_qbol_flux(flux_group):
     """Turn a group of fluxes into a quasi-bolometric flux"""
-    return get_quasi_bolometric_flux(TrapezoidalIntegralCalculator(),
-                                     uncertainty_calculator_trapezoidal,
+    return get_quasi_bolometric_flux(SplineIntegralCalculator(),
+                                     uncertainty_calculator_spline,
                                      flux_group)
--- tests/integration/test_qbol_lightcurve.py
+++ tests/integration/test_qbol_lightcurve.py
@@ -10,16 +10,16 @@ from superbol import lum
 from superbol import extinction
 
 dirname = os.path.dirname(__file__)
-sn2000cb_extinction = os.path.join(dirname, '../../data/sn2000cb_extinction.dat')
-extinction_table = Table.read(sn2000cb_extinction, format = 'ascii')
-sn00cb_photometry = os.path.join(dirname, '../../data/SN2000cb.json')
+sn2018hna_extinction = os.path.join(dirname, '../../data/sn2018hna_extinction.dat')
+extinction_table = Table.read(sn2018hna_extinction, format = 'ascii')
+sn18hna_photometry = os.path.join(dirname, '../../data/SN2018hna.json')
 
 class TestQuasiBolometricLightcurve(unittest.TestCase):
 
     def setUp(self):
-        sn00cb_osc_photometry = read_osc.retrieve_osc_photometry('SN2000cb', path=sn00cb_photometry)
+        sn18hna_osc_photometry = read_osc.retrieve_osc_photometry('SN2018hna', path=sn18hna_photometry)
         fluxes = []
-        for photometry_dict in sn00cb_osc_photometry:
+        for photometry_dict in sn18hna_osc_photometry:
             try:
                 observed_magnitude = read_osc.get_observed_magnitude(photometry_dict)
                 extinction_value = extinction.get_extinction_by_name(extinction_table, observed_magnitude.band)
@@ -27,11 +27,11 @@ class TestQuasiBolometricLightcurve(unittest.TestCase):
                 fluxes.append(observed_magnitude.convert_to_flux())
             except:
                 pass
-        
+        #TODO check distance
         distance = lum.Distance(3.0E7 * 3.086E18, 7.0E6 * 3.086E18)
-        self.lc_00cb = lightcurve.calculate_lightcurve(fluxes, distance, lqbol.calculate_qbol_flux)
-
-    def test_00cb_qbol_lightcurve(self):
+        self.lc_18hna = lightcurve.calculate_lightcurve(fluxes, distance, lqbol.calculate_qbol_flux)
+        #TODO check time values
+    def test_18hna_qbol_lightcurve(self):
         print("")
-        for luminosity in self.lc_00cb:
+        for luminosity in self.lc_18hna:
             print("{0:9.2f}, {1:5.2f}, 4.1, {2:4.2E}, {3:4.2E}".format(luminosity.time + 2400000.5, luminosity.time + 2400000.5 - 2451656, luminosity.value, luminosity.uncertainty))
