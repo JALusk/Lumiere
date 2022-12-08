Fixed the first failing test in the spline integral calculation
The problem: the expected from the test was still the one from the
trapezoidal integral calculator. The spline uncertainty
calculation works differently, and so the result shouldn't be the
same as the one from the trapezoidal integration.

Specifically, with the data (in (x , y +/- dy) notation):

(0, 100 +/- 10)
(1, 100 +/- 10)

the expected integral between the data is int = 100.
If you then integrate between the upper limits of the data, you
get 110. The difference (which the code currently computes as the
uncertainty in the integral) is 110 - 100 = 10.

superbol/fqbol.py
@@ -61,10 +61,7 @@ class SplineIntegralCalculator(object):
         self._sort_fluxes_by_wavelength(fluxes)
         flux_list = self._get_flux_list(fluxes)
         wavelength_list = self._get_wavelength_list(fluxes)
-        print("Wavelength list: ", wavelength_list)
-        print("Flux list: ", flux_list)
         spline = scipy.interpolate.CubicSpline(wavelength_list, flux_list, bc_type='natural') 
-        #print([x for x in spline])
         integrated_spline = float(spline.integrate(wavelength_list[0], wavelength_list[-1]))
         return integrated_spline
 
@@ -122,7 +119,11 @@ def uncertainty_calculator_spline(fluxes):
     uncertainty_spline = scipy.interpolate.CubicSpline(wavelength_list, flux_plus_uncertainty, bc_type='natural')
     uncertainty_integrated = float(uncertainty_spline.integrate(wavelength_list[0], wavelength_list[-1]))
 
-    flux_spline = scipy.interpolate.CubicSpline(wavelength_list, flux_list, bc_type='natural') 
+    flux_spline = scipy.interpolate.CubicSpline(wavelength_list, flux_list, bc_type='natural')
+    print(flux_spline.c)
+    print(flux_spline.x)
+    deriv = flux_spline.derivative()
+    print(deriv.c)
     flux_integrated = float(flux_spline.integrate(wavelength_list[0], wavelength_list[-1]))
     
     qbolflux_uncertainty = uncertainty_integrated - flux_integrated
@@ -132,7 +133,6 @@ def uncertainty_calculator_spline(fluxes):
 
 def calculate_qbol_flux(flux_group):
     """Turn a group of fluxes into a quasi-bolometric flux"""
-    print(flux_group[0].time)
     return get_quasi_bolometric_flux(SplineIntegralCalculator(),
                                      uncertainty_calculator_spline,
                                      flux_group)