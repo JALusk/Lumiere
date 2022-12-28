a8a34ee05002d5738b3e26f82196601a1fcaece0
Hypatia Meraviglia <hbarry@cub.uca.edu>
2022-12-28 16:42:01 -0600

    Created calc_wiggled.py, which wiggles a submitted sed 1000 times and returns the average and STDEV of the qbol Flux

---
+++ superbol/calc_wiggled.py
@@ -0,0 +1,23 @@
+import numpy as np
+from superbol import flux_wiggler
+from superbol import fqbol
+
+num_wiggled_seds = 10
+
+def wiggle_fluxes_n_times(sed):
+    wiggled_qbol_fluxes = [None] * num_wiggled_seds
+    for i in range(num_wiggled_seds):
+        #Make a copy of the original sed
+        sed_copy = flux_wiggler.copy_flux_list(sed)
+        #Wiggle each flux at each time point in sed
+        wiggled_sed = flux_wiggler.wiggle_fluxes(sed_copy)
+        #Integrate wiggled sed to get wiggled qbol flux
+        wiggled_qbol_fluxes[i] = fqbol.SplineIntegralCalculator().calculate(wiggled_sed)
+    return wiggled_qbol_fluxes
+
+def calc_avg_stdev(sed):
+    average_qbol_flux = np.average(wiggle_fluxes_n_times(sed))
+    print("Average wiggled quasibolometric flux: ", average_qbol_flux)
+    stdev_qbol_flux = np.std(wiggle_fluxes_n_times(sed))
+    print("STDEV of wiggled quasibolometric fluxes: ", stdev_qbol_flux)
+    return [average_qbol_flux, stdev_qbol_flux]
--- superbol/flux_wiggler.py
+++ superbol/flux_wiggler.py
@@ -3,20 +3,23 @@ import random
 from superbol import lightcurve
 
 #Make a copy of the existing list of fluxes
-def copy_flux_list(fluxes, distance, flux_calculator):
-    flux_list_copy = lightcurve.calculate_lightcurve(fluxes, distance, flux_calculator)
-    #Should this call lightcurve.calculate_lightcurve or another function?
-    #Is there a more useful + accurate name than flux_list_copy? 
-    print("Copied flux list: ", flux_list_copy)
+def copy_flux_list(sed):
+    flux_list_copy = [None] * len(sed)
+    for i in range(len(sed)):
+        flux_list_copy[i] = sed[i]
+    for flux in flux_list_copy:
+        print("Copied flux list: ", flux)
     return flux_list_copy
 
 #Wiggle them
 def wiggle_fluxes(flux_list_copy):
     for flux in flux_list_copy:
         top = flux.flux + flux.flux_uncertainty
+        print("Top: ", top)
         bottom = flux.flux - flux.flux_uncertainty
+        print("Bottom; ", bottom)
         #Where does df come from? lightcurve.calculate_lightcurve doesn't retunr an error bar...
         flux.flux = random.uniform(bottom, top)
-    print("Wiggled fluxes: ", flux_list_copy)
+    #print("Wiggled fluxes: ", flux_list_copy)
     return flux_list_copy
 
