6395abcf49a5918bf8b26bd979814019babfc256
Hypatia Meraviglia <hbarry@cub.uca.edu>
2023-04-20 15:42:17 -0500

    Separated MPI and calc_wiggled, now running

---
+++ superbol/mpi_shell.py
@@ -0,0 +1,91 @@
+#mpi_shell.py
+
+import numpy as np
+from superbol import calc_wiggled
+from superbol import mag2flux
+
+import time
+from mpi4py import MPI
+from mpi4py.MPI import COMM_WORLD
+from types import FunctionType
+from multiprocessing import Pool
+
+#Data from test_calc_wiggled.py
+flux01 = mag2flux.MonochromaticFlux(100, 2, 1, 0)
+flux02 = mag2flux.MonochromaticFlux(200, 2, 2, 0)
+flux03 = mag2flux.MonochromaticFlux(150, 2, 3, 0)
+sed = [flux01, flux02, flux03]
+
+#MPI set-up, see p. 302 in Eff. Comp.
+class Pool(object):
+    """Process pool using MPI."""
+    def __init__(self):
+        self.f = None
+        self.P = COMM_WORLD.Get_size() #number of processors?
+        self.rank = COMM_WORLD.Get_rank()
+
+    def wait(self):
+        if self.rank == 0:
+            raise RuntimeError("Proc 0 cannot wait!")
+        status = MPI.Status()
+        while True:
+            task = COMM_WORLD.recv(source=0, tag=MPI.ANY_TAG, status=status)
+            if not task:
+                break
+            if isinstance(task, FunctionType):
+                self.f = task
+                continue
+            result = self.f(task)
+            COMM_WORLD.isend(result, dest=0, tag=status.tag)
+
+    def map(self, f, tasks):
+        N = len(tasks)
+        P = self.P #number of worker ranks
+        Pless1 = P - 1
+        if self.rank != 0:
+            self.wait()
+            return
+
+        if f is not self.f:
+            self.f = f
+            requests = []
+            for p in range(1, self.P):
+                r = COMM_WORLD.isend(f, dest=p)
+                requests.append(r)
+            MPI.Request.waitall(requests)
+
+        requests = []
+        for i, task in enumerate(tasks):
+            r = COMM_WORLD.isend(task, dest=(i%Pless1)+1, tag=i)
+            requests.append(r)
+        MPI.Request.waitall(requests)
+
+        results = []
+        for i in range(N):
+            result = COMM_WORLD.recv(source=(i%Pless1)+1, tag=i)
+            results.append(result)
+        return results
+
+    def __del__(self):
+        if self.rank == 0:
+            for p in range(1, self.P):
+                COMM_WORLD.isend(False, dest=p)
+
+
+pool = Pool()
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+#start timer
+start = time.time()
+if __name__ == '__main__':
+    wiggled_qbol_fluxes = calc_wiggled.wiggle_fluxes_n_times(sed)
+    #print("\nNumber of processors: ", processors)
+    if rank == 0:
+        average, stdev = calc_wiggled.calc_avg_stdev(wiggled_qbol_fluxes)
+        print("Average across all wiggles: ", average)
+        print("STDEV across all wiggles: ", stdev)
+stop = time.time()
+runtime = stop - start
+print("\nRuntime (sec) is ", runtime)
--- tests/unit/test_calc_wiggled.py
+++ tests/unit/test_calc_wiggled.py
@@ -12,7 +12,7 @@ class TestCalcWiggled(unittest.TestCase):
         self.flux02 = mag2flux.MonochromaticFlux(200, 2, 2, 0)
         self.flux03 = mag2flux.MonochromaticFlux(150, 2, 3, 0)
         self.sed01 = [self.flux01, self.flux02, self.flux03]
-    """
+    
     def test_wiggled_qbol_fluxes_is_list(self):
         result = calc_wiggled.wiggle_fluxes_n_times(self.sed01)
         print("result is ", result)
@@ -22,36 +22,25 @@ class TestCalcWiggled(unittest.TestCase):
         result = calc_wiggled.wiggle_fluxes_n_times(self.sed01)
         print("result is ", result)
         self.assertEqual(len(result[0]), calc_wiggled.num_wiggled_seds)
-    """
+    
     def test_average_qbol_flux_above_min(self):
-        result = calc_wiggled.wiggle_in_parallel(self.sed01)[0]
+        result = calc_wiggled.calc_avg_stdev(self.sed01)[0]
         print("Average wiggled quasibolometric flux: ", result)
         min_avg = 339.75 #calculated directly from test values with spline
         self.assertGreaterEqual(result, min_avg)
 
     def test_average_qbol_flux_below_max(self):
-        result = calc_wiggled.wiggle_in_parallel(self.sed01)[0]
+        result = calc_wiggled.calc_avg_stdev(self.sed01)[0]
         max_avg = 347.75 #calculated directly from test values with spline
         self.assertLessEqual(result, max_avg)
 
     def test_stdev_above_min(self):
-        result = calc_wiggled.wiggle_in_parallel(self.sed01)[1]
+        result = calc_wiggled.calc_avg_stdev(self.sed01)[1]
         print("STDEV of wiggled quasibolometric fluxes: ", result)
         min_stdev = 0
         self.assertGreaterEqual(result, min_stdev)
     
     def test_stdev_below_max(self):
-        result = calc_wiggled.wiggle_in_parallel(self.sed01)[1]
+        result = calc_wiggled.calc_avg_stdev(self.sed01)[1]
         max_stdev = 1600
         self.assertLessEqual(result, max_stdev)
-    """
-    def test_wiggling_runtime_above_min(self):
-        result = calc_wiggled.wiggle_in_parallel(self.sed01)
-        min_runtime = 0
-        self.assertGreaterEqual(result, min_runtime)
-    
-    def test_num_processors_above_min(self):
-        result = calc_wiggled.wiggle_fluxes_n_times(self.sed01)[1]
-        min_processors = 2
-        self.assertGreaterEqual(result, min_processors)
-    """
